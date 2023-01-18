2c42951e9ab8289c45f9bc0fd3b5204faa871928
Shawn Wildermuth <shawn@wildermuth.com>
2023-01-18 00:22:54 -0500

    Remove test folder

--- aspnetvite/test/Models/FilmResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BechdelDataServer.Models;
-
-public record FilmResult(int Count, 
-    int PageCount, 
-    int CurrentPage, 
-    IEnumerable<Film>? Results)
-{
-  public static FilmResult Default = new FilmResult(0, 0, 0, null);
-}
--- aspnetvite/test/client/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  import HelloWorld from '../components/HelloWorld.vue';
-</script>
-
-<template>
-  <main>
-    <hello-world></hello-world>
-  </main>
-</template>
