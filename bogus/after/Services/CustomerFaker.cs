d8f847b3812559834b11b12dd77f98ff3fbe4546
Shawn Wildermuth <shawn@wildermuth.com>
2023-01-29 18:32:09 -0600

    Add class-based Fakers

---
+++ bogus/after/Services/CustomerFaker.cs
@@ -0,0 +1,23 @@
+using Bogus;
+using FakingIt.Entities;
+
+namespace FakingIt.Services;
+
+public class CustomerFaker : Faker<Customer>
+{
+  AddressFaker _addrFaker = new AddressFaker();
+
+	public CustomerFaker()
+  {
+    var id = 1;
+
+	  UseSeed(1969) // Use any number
+    .RuleFor(c => c.Id, _ => id++)
+    .RuleFor(c => c.CompanyName, f => f.Company.CompanyName())
+    .RuleFor(c => c.ContactName, f => f.Name.FullName())
+    .RuleFor(c => c.Phone, f => f.Phone.PhoneNumberFormat())
+    .RuleFor(c => c.Address, _ => _addrFaker.Generate(1)
+                                            .First()
+                                            .OrNull(_, .1f));
+  }
+}
