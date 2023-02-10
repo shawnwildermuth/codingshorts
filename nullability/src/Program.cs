f73c1b44b4b13c791e4580d9b3640c7f54cb2411
Shawn Wildermuth <shawn@wildermuth.com>
2023-02-10 03:24:03 -0500

    Nullability Code

---
+++ nullability/src/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+//#nullable enable
+string? x = "";
+
+User<string> user = new("Hello");
+
+WriteLine(user.Name!.Length);
+
+WriteLine(x is null ? "Null" : "Not Null");
+
+class User<T> where T : notnull
+{
+  public int Id { get; set; }
+  public T? Name { get; set; }
+
+  public User(T name)
+  {
+    Name = name;
+  }
+}
+
+//#nullable disable
