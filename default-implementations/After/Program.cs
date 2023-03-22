742e661714885bf8652760f8e7c1c625b1ec66af
Shawn Wildermuth <shawn@wildermuth.com>
2023-03-22 01:57:09 -0400

    Adding Interface Default Implementatio

---
+++ default-implementations/After/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+var math = new Math();
+var m = (IAddition)math;
+var s = (ISubtraction)math;
+WriteLine(m.Add(4, 5));
+WriteLine(s.Subtract(4, 5));
+
+public interface IAddition
+{
+  int Add(int x, int y) => x + y;
+}
+
+public interface ISubtraction
+{ 
+  int Subtract(int x, int y) => x - y;
+}
+
+public class Math : Object, IAddition, ISubtraction
+{
+  public int Add(int x, int y) => (x + y) * 10;
+  public int Subtract(int x, int y) => (x - y) * 10;
+}
---
+++ default-implementations/Before/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+var m = new Math();
+WriteLine(m.Add(4, 5));
+WriteLine(m.Subtract(4, 5));
+
+public class Math
+{
+  public int Add(int x, int y) => x + y;
+  public int Subtract(int x, int y) => x - y;
+}
