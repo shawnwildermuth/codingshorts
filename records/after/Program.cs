94f1b0c74b90ef969082711321de87b1322de21a
Shawn Wildermuth <shawn@wildermuth.com>
2023-04-02 17:24:04 -0400

    Adding Records

---
+++ records/after/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+using Foo;
+
+WriteLine("Starting...");
+
+var product = new RetailProduct(1, "Tomato", 2.99m, null, 10);
+var product2 = new RetailProduct(1, "Tomato", 2.99m, "A red tomato", 5);
+var product3 = product with { Price = 1.99m };
+
+WriteLine($"Are Equal? {product == product2}");
+WriteLine(product.ToString());
+
+namespace Foo
+{
+  public abstract record Product(int Id,
+                        string Name,
+                        decimal Price,
+                        string? Description = null)
+  {
+    public bool IsLowPrice() => Price < 100;
+  };
+
+  public record RetailProduct(int Id,
+    string Name,
+    decimal Price,
+    string? Description,
+    int NumInStock) : Product(Id, Name, Price, Description);
+}
---
+++ records/before/Program.cs
@@ -0,0 +1,4 @@
+﻿using static System.Console;
+
+WriteLine("Starting...");
+
