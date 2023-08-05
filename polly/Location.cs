ebfbfb3772f4dcb8692ee22cbe5eb73c9168e6ef
Shawn Wildermuth <shawn@wildermuth.com>
2023-08-05 00:03:32 -0400

    PollyBefore

---
+++ polly/Location.cs
@@ -0,0 +1,9 @@
+namespace WhatIsPolly;
+
+public class Location
+{
+  public string? Name { get; set; }
+  public double Longitude { get; set; }
+  public double Latitude { get; set; }
+}
+
---
+++ polly/Program.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using static System.Console;
+
+var client = new RestClient("https://arest.me/api");
+
+var request = new RestRequest("/data/flaky"); // Get By Default
+
+var count = 0;
+
+do
+{
+  var result = await client.ExecuteAsync<string>(request);
+
+  count++;
+
+  WriteLine(result.IsSuccessful ? "Succeeded" : "Failed");
+
+} while (count < 10);
+
+
