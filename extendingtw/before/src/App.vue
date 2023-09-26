4944c5c5d7f921415882d80a12a67dbe957987e8
Shawn Wildermuth <shawn@wildermuth.com>
2023-09-26 18:20:19 -0400

    extendingtw

---
+++ extendingtw/before/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-2 container mx-auto bg-gray-200">
+    <header class="flex justify-between">
+      <div class="text-xl font-bold">Extending Vue</div>
+      <ul class="flex">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/simple">Simple Forms</router-link></li>
+        <li><router-link to="/niche">Niche Forms</router-link></li>
+      </ul>
+    </header>
+    <div class="w-full p-4">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+ul li {
+  @apply font-bold p-2 bg-green-800 text-white mr-2 hover:bg-green-700;
+}
+
+</style>
---
+++ extendingtw/before/src/views/NicheForms.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts"></script>
+
+<template>
+  <form novalidate>
+    <div>
+      <label>Empty</label>
+      <input />
+      <label>Text</label>
+      <input type="text" />
+      <label>Email</label>
+      <input type="email" />
+      <label>Url</label>
+      <input type="url" />
+      <label>Password</label>
+      <input type="password" />
+      <label>Text Area</label>
+      <textarea rows="4"></textarea>
+      <label>Select</label>
+      <select>
+        <option>Foo</option>
+        <option>Bar</option>
+        <option>Quux</option>
+      </select>
+      <label>Number</label>
+      <input type="number" />
+      <label>Date</label>
+      <input type="date" />
+      <label>DateTime-Local</label>
+      <input type="datetime-local" />
+      <label>Month</label>
+      <input type="month" />
+      <label>Search</label>
+      <input type="search" />
+      <label>Tel</label>
+      <input type="tel" />
+      <label>Time</label>
+      <input type="time" />
+      <label>Week</label>
+      <input type="week" />
+      <label>Select - Multi</label>
+      <select multiple>
+        <option>Foo</option>
+        <option>Bar</option>
+        <option>Quux</option>
+      </select>
+      <label> <input type="checkbox" /> Checkbox </label>
+      <label>Radio Buttons</label>
+      <label>
+        <input type="radio" value="One" name="aRadio" />
+        One</label
+      >
+      <label>
+        <input type="radio" value="Two" name="aRadio" />
+        Three</label
+      >
+      <label>
+        <input type="radio" value="Three" name="aRadio" />
+        Three</label
+      >
+      <button type="submit">Submit</button>
+      <button>Cancel</button>
+    </div>
+  </form>
+</template>
