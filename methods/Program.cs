8e8abf7a0240b1e3f5627871ce7c7b0c6b179d36
shawnwildermuth <shawn@wildermuth.com>
2024-05-01 03:02:14 -0400

    Methods

---
+++ methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+var maths = new Maths();
+
+var addFunction = Maths.Add;
+
+var doAddition = () => {
+  return Maths.Add(6,6) == 12;
+};
+
+var doAction = (string foo) => WriteLine(foo);
+
+doAction("Hello World");
+
+WriteLine(addFunction(2,3));
+WriteLine(doAddition());
+
+Maths.RunThis(() => true);
+
+public class Maths
+{
+  public static int Add(int x , int y) => x + y;
+
+  public static bool RunThis(Func<bool> operation) => operation();
+}
