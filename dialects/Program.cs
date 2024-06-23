526775dac74355f78423f9736407af3d8349560a
shawnwildermuth <shawn@wildermuth.com>
2024-06-23 04:43:45 -0400

    Update Dialects

---
+++ dialects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+  public static void Main()
+  {
+    string[] array = new string[5];
+    array[0] = "Hello";
+    array[1] = "World";
+
+    foreach (string message in array)
+    {
+      Console.WriteLine(message);
+    }
+  }
+}
