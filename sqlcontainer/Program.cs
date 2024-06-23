0e4ded55609dce93696b323c8e1c4e1ceeceef4b
shawnwildermuth <shawn@wildermuth.com>
2024-06-23 16:54:18 -0400

    More changes before the container

--- sqlcontainer/Program.cs
+++ sqlcontainer/Program.cs
@@ -1,6 +1,8 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using SqlDev.Data;
 using SqlDev.StoreFakers;
+using static Microsoft.AspNetCore.Http.Results;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +12,21 @@ builder.Services.AddDbContext<StoreContext>(cfg => {
   cfg.UseSqlServer(builder.Configuration.GetConnectionString("StoreDb"));
 });
 
+builder.Services.ConfigureHttpJsonOptions(options => {
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.SerializerOptions.WriteIndented = true;
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/customers", async (StoreContext ctx) => {
+  var results = await ctx.Customers
+  .Include(c => c.Address)
+  .Include(c => c.Orders)
+  .OrderBy(c => c.CompanyName)
+  .ToListAsync();
+  
+  return Ok(results);
+});
 
 app.Run();
