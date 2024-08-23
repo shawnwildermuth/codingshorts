7fcbc096a8fe23b33018ae4169f6170597e5f7fa
shawnwildermuth <shawn@wildermuth.com>
2024-08-23 17:54:59 -0400

    Simplified example

--- testingwithcontainers/AddressBook.Tests/ApiTests.cs
+++ testingwithcontainers/AddressBook.Tests/ApiTests.cs
@@ -3,34 +3,57 @@ using AddressBook.Api.Data.Entities;
 using AddressBook.Api.Models;
 using AddressBook.Apis;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
 namespace AddressBook.Tests;
 
 public class ApiTests
 {
-  private IBookRepository _repository;
+  private IServiceProvider Services = RegisterServices();
 
-  public ApiTests()
+  static ServiceProvider RegisterServices()
   {
-    IEnumerable<BookEntry> entries = new BookEntryFaker(new AddressFaker()).Generate(10);
+    return new ServiceCollection().AddTransient<IBookRepository>(_ =>
+    {
+      List<BookEntry> entries = new BookEntryFaker(new AddressFaker()).Generate(10);
 
-    var mock = new Mock<IBookRepository>();
-    mock.Setup(p => p.GetAllEntries())
-      .Returns(() => Task.FromResult(entries));
-    mock.Setup(p => p.GetEntry(It.IsAny<int>()))
-      .Returns((int id) => Task.FromResult(entries.Where(e => e.Id == id).FirstOrDefault()));
+      var mock = new Mock<IBookRepository>();
+      mock.Setup(p => p.GetAllEntries())
+        .Returns(() => Task.FromResult(entries.AsEnumerable()));
 
-    _repository = mock.Object;
+      mock.Setup(p => p.GetEntry(It.IsAny<int>()))
+        .Returns((int id) => Task.FromResult(entries.Where(e => e.Id == id).FirstOrDefault()));
+
+      mock.Setup(p => p.Add(It.IsAny<BookEntry>())).Callback<BookEntry>(entry =>
+      {
+        entry.Id = entries.Max(e => e.Id) + 1;
+        entries.Add(entry);
+      }).Verifiable();
+
+      mock.Setup(p => p.SaveAllAsync())
+        .Returns(() => Task.FromResult(true));
+
+      mock.Setup(p => p.Remove(It.IsAny<BookEntry>()))
+        .Callback<BookEntry>(entry =>
+        {
+          entries.Remove(entry);
+        }).Verifiable();
+
+      return mock.Object;
+    }).BuildServiceProvider();
   }
 
   [Fact]
   public async Task CanGetAllEntries()
   {
-    var result = await EntriesApi.GetEntries(_repository);
+    var repository = Services.GetRequiredService<IBookRepository>();
+
+    var result = await EntriesApi.GetEntries(repository);
 
     Assert.IsAssignableFrom<Ok<IEnumerable<BookEntryModel>>>(result);
-    
+
     var data = ((Ok<IEnumerable<BookEntryModel>>)result).Value;
     Assert.NotNull(data);
     Assert.NotEmpty(data);
@@ -39,15 +62,72 @@ public class ApiTests
   [Fact]
   public async Task CanGetEntry()
   {
-    var result = await EntriesApi.GetEntries(_repository);
+    var repository = Services.GetRequiredService<IBookRepository>();
+
+    var result = await EntriesApi.GetEntries(repository);
     var data = ((Ok<IEnumerable<BookEntryModel>>)result).Value?.First();
 
-    var entryResult = await EntriesApi.GetEntry(_repository, data!.Id);
+    var entryResult = await EntriesApi.GetEntry(repository, data!.Id);
 
     Assert.IsAssignableFrom<Ok<BookEntryModel>>(entryResult);
 
     var entry = ((Ok<BookEntryModel>)entryResult).Value;
     Assert.NotNull(entry);
     Assert.Equivalent(data, entry);
+  }
+
+  [Fact]
+  public async Task CanSaveEntry()
+  {
+    var repository = Services.GetRequiredService<IBookRepository>();
+
+    var model = new BookEntryModel
+    {
+      CompanyName = "Foo Bar",
+      FirstName = "Beth",
+      LastName = "Smith",
+      Email = "bsmith@aol.com",
+      HomePhone = "404-555-1212"
+    };
+    var result = await EntriesApi.PostEntry(repository, model);
+    var data = ((Created<BookEntryModel>)result).Value;
+
+    Assert.NotNull(data);
+    Assert.Equal("Beth", data.FirstName);
+    Assert.Equal("bsmith@aol.com", data.Email);
+    Assert.True(data.Id != default);
+
+    var entryResult = await EntriesApi.GetEntry(repository, data.Id);
+
+    Assert.IsAssignableFrom<Ok<BookEntryModel>>(entryResult);
+
+    var entry = ((Ok<BookEntryModel>)entryResult).Value;
+    Assert.NotNull(entry);
+    Assert.Equivalent(data, entry);
+  }
+
+  [Fact]
+  public async Task CanDeleteEntry()
+  {
+    var repository = Services.GetRequiredService<IBookRepository>();
+
+    var model = new BookEntryModel
+    {
+      CompanyName = "Another Name",
+      FirstName = "John",
+      LastName = "Jenny",
+      Email = "jjenny@aol.com",
+      HomePhone = "404-555-1213"
+    };
+    var result = await EntriesApi.PostEntry(repository, model);
+    var data = ((Created<BookEntryModel>)result).Value;
+
+    Assert.NotNull(data);
+
+    var deleteResult = await EntriesApi.DeleteEntry(repository, data.Id);
+    Assert.IsAssignableFrom<Ok>(deleteResult);
+
+    var entryResult = await EntriesApi.GetEntry(repository, data.Id);
+    Assert.IsAssignableFrom<NotFound>(entryResult);
   }
 }
