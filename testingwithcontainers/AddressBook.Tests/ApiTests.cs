c6f6165e9144a1c81e4f286877fa55c53bd18aba
shawnwildermuth <shawn@wildermuth.com>
2024-10-02 17:02:35 -0400

    Testing with containers

--- testingwithcontainers/AddressBook.Tests/ApiTests.cs
+++ testingwithcontainers/AddressBook.Tests/ApiTests.cs
@@ -4,51 +4,20 @@ using AddressBook.Api.Models;
 using AddressBook.Apis;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using Testcontainers.MsSql;
 
 namespace AddressBook.Tests;
 
-public class ApiTests
+public class ApiTests(DatabaseFixture db) : IClassFixture<DatabaseFixture>
 {
-  private IServiceProvider Services = RegisterServices();
-
-  static ServiceProvider RegisterServices()
-  {
-    return new ServiceCollection().AddTransient<IBookRepository>(_ =>
-    {
-      List<BookEntry> entries = new BookEntryFaker(new AddressFaker()).Generate(10);
-
-      var mock = new Mock<IBookRepository>();
-      mock.Setup(p => p.GetAllEntries())
-        .Returns(() => Task.FromResult(entries.AsEnumerable()));
-
-      mock.Setup(p => p.GetEntry(It.IsAny<int>()))
-        .Returns((int id) => Task.FromResult(entries.Where(e => e.Id == id).FirstOrDefault()));
-
-      mock.Setup(p => p.Add(It.IsAny<BookEntry>())).Callback<BookEntry>(entry =>
-      {
-        entry.Id = entries.Max(e => e.Id) + 1;
-        entries.Add(entry);
-      }).Verifiable();
-
-      mock.Setup(p => p.SaveAllAsync())
-        .Returns(() => Task.FromResult(true));
-
-      mock.Setup(p => p.Remove(It.IsAny<BookEntry>()))
-        .Callback<BookEntry>(entry =>
-        {
-          entries.Remove(entry);
-        }).Verifiable();
-
-      return mock.Object;
-    }).BuildServiceProvider();
-  }
 
   [Fact]
   public async Task CanGetAllEntries()
   {
-    var repository = Services.GetRequiredService<IBookRepository>();
+    var repository = db.Services.GetRequiredService<IBookRepository>();
 
     var result = await EntriesApi.GetEntries(repository);
 
@@ -62,7 +31,7 @@ public class ApiTests
   [Fact]
   public async Task CanGetEntry()
   {
-    var repository = Services.GetRequiredService<IBookRepository>();
+    var repository = db.Services.GetRequiredService<IBookRepository>();
 
     var result = await EntriesApi.GetEntries(repository);
     var data = ((Ok<IEnumerable<BookEntryModel>>)result).Value?.First();
@@ -79,7 +48,7 @@ public class ApiTests
   [Fact]
   public async Task CanSaveEntry()
   {
-    var repository = Services.GetRequiredService<IBookRepository>();
+    var repository = db.Services.GetRequiredService<IBookRepository>();
 
     var model = new BookEntryModel
     {
@@ -109,7 +78,7 @@ public class ApiTests
   [Fact]
   public async Task CanDeleteEntry()
   {
-    var repository = Services.GetRequiredService<IBookRepository>();
+    var repository = db.Services.GetRequiredService<IBookRepository>();
 
     var model = new BookEntryModel
     {
@@ -130,4 +99,5 @@ public class ApiTests
     var entryResult = await EntriesApi.GetEntry(repository, data.Id);
     Assert.IsAssignableFrom<NotFound>(entryResult);
   }
+
 }
---
+++ testingwithcontainers/AddressBook.Tests/DatabaseFixture.cs
@@ -0,0 +1,56 @@
+﻿using AddressBook.Api.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+
+namespace AddressBook.Tests;
+public class DatabaseFixture : IAsyncLifetime
+{
+  public IServiceProvider Services;
+  private MsSqlContainer _container;
+
+  public DatabaseFixture()
+  {
+    _container = new MsSqlBuilder()
+      .Build();
+
+    var coll = new ServiceCollection();
+
+    coll.AddTransient<IConfiguration>(_ =>
+    {
+      var configs = new List<KeyValuePair<string, string?>>
+      {
+        new ("ConnectionStrings:AddressBookDb", _container.GetConnectionString())
+      };
+
+      return new ConfigurationBuilder()
+      .AddInMemoryCollection(configs)
+      .Build();
+    });
+
+    coll.AddDbContext<BookContext>();
+    coll.AddTransient<IBookRepository, BookRepository>();
+    coll.AddTransient<BookEntryFaker>();
+    coll.AddTransient<AddressFaker>();
+
+    Services = coll.BuildServiceProvider();
+
+  }
+  public async Task InitializeAsync()
+  {
+    await _container.StartAsync();
+    var context = Services.GetRequiredService<BookContext>();
+    await context.Database.EnsureCreatedAsync();
+  }
+
+  public async Task DisposeAsync()
+  {
+    await _container.StopAsync();
+  }
+
+}
