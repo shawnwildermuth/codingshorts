cf6e142c0ea669fde7eac18963371c4a1908602a
shawnwildermuth <shawn@wildermuth.com>
2024-10-14 01:39:37 -0400

    Before extendedtw

--- extendingtw/before/src/main.ts
+++ extendingtw/before/src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './styles/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- extendingtw/test/src/main.ts
+++ extendingtw/test/src/main.ts
@@ -1,4 +1,4 @@
-import './styles/main.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
